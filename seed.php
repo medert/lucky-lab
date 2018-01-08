2fde92e349518dee7824af92e2e05b2edc993553
Meder <m.toktosunov@icloud.com>
2018-01-07 22:20:17 -0500

    corrected db connection on seed.php

--- seed.php
+++ seed.php
@@ -2,7 +2,7 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
+$servername = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $dbname = substr($url["path"], 1);
